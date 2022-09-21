29e97a22324714e249c26d2aaa606343adb7f5ca
shinshin86 <beagles1986@gmail.com>
2022-09-22 07:53:42 +0900

    Fix react warning

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <ChakraProvider>
       <App />
     </ChakraProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
